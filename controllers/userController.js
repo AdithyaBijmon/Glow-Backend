a27733b6c31a7998100e8f7e43c5b058bf627256
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-10-30 23:47:57 +0530

    middleware updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,17 @@
 const users = require("../models/userModel")
+const jwt = require('jsonwebtoken')
 
-exports.registerController = async (req,res)=>{
+exports.registerController = async (req, res) => {
     console.log("Inside register controller")
 
-    const {username,email,password} = req.body
+    const { username, email, password } = req.body
 
-    try{
-        const existingUser = await users.findOne({email})
-        if(existingUser){
+    try {
+        const existingUser = await users.findOne({ email })
+        if (existingUser) {
             res.status(409).json("User Already exists!")
         }
-        else{
+        else {
             const newUser = new users({
                 username,
                 email,
@@ -21,34 +22,37 @@ exports.registerController = async (req,res)=>{
             res.status(200).json(newUser)
         }
     }
-    catch(err){
-        res.status(500).json("Something Wrong",err)
+    catch (err) {
+        res.status(500).json("Something Wrong", err)
     }
 }
 
-exports.loginController = async (req,res)=>{
+exports.loginController = async (req, res) => {
     console.log("Inside login controller")
 
-    const {email,password} = req.body
+    const { email, password } = req.body
 
-    try{
-        const existingUser = await users.findOne({email})
-        if(!existingUser){
-            res.status(404).json("You have'nt registered.Please register first and login.")
-        }
-        else{
-            if(existingUser.password==password){
-                res.status(200).json("Logged in successfully.")
+     try {
+        const existingUser = await users.findOne({ email })
+
+        if (existingUser) {
+            if (existingUser.password == password) {
+                // token
+                const token = jwt.sign({ userMail: existingUser.email,role:existingUser.role }, process.env.JWTSECRET)
+                res.status(200).json({ user: existingUser, token })
             }
-            else{
+            else {
                 res.status(401).json("Invalid email or password")
             }
-            
 
         }
+        else {
+
+            res.status(404).json("Account does not exist!")
+        }
     }
-    catch(err){
-        res.status(500).json("Something Wrong",err)
+    catch (err) {
+        res.status(500).json(err)
     }
 }
 
---
+++ middlewares/jwtMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+const jwt = (req,res,next)=>{
+    console.log("Inside jwt middleware")
+    const token = req.headers.authorization.split(" ")[1]
+
+    try{
+        const jwtResponse = jwt.verify(token,process.env.JWTSECRET)
+        req.payload = jwtResponse.userMail
+        next()
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
+}
